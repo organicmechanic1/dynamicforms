095a9f4bb2485d39a9191ffebfba81370c296e94
Erick G <lmterickg@cox.net>
2017-03-14 20:23:34 -0700

    final commit

--- main.js
+++ main.js
@@ -5,39 +5,39 @@ $(document).ready(function(){
 		var formHTML = ''
 
 		formData.forEach(function(field){
-			if(field.type === 'select'){
-				formHTML += `<select type="${field.type}" placeholder="${field.label}" id="${field.id}" ?>`}
-					if {formHTML += `placeholder=`}
+			if (field.type === "text" || field.type === "email" || field.type === "tel") {
+				formHTML += `<div class="field"><i class = "fa ${field.icon}" aria-hidden = "true" > </i>
+				<input type "${field.type}" placeholder = "${field.label}" id = "${field.id}" name="${field.id}"/></div>`
 
+			}
 
+			if (field.type === "select"){
+				formHTML += `<select id = "${field.id}" name="${field.id}">
+					<option value=''> ${field.label}></option> `
 
+					field.options.forEach(function(option){
+						formHTML += `<option value="${option.value}">${option.label}</option>`
+					})
 
+					formHTML += `</select>`
 
+			}
 
+			if (field.type === "textarea"){
+				formHTML += `<div class="textarea"><i class = "fa ${field.icon}" aria-hidden = "true" ></i><textarea name="${field.id}" id="${field.id}" placeholder="${field.label}"></textarea></div>`
+			}
 
-
-
-			} else {
-			formHTML += `<i class="fa ${field.icon}" /><input type="${field.type}" placholder="${field.label}" id="${field.id}" />`}
 		})
+
+		formHTML += `<div class="button"><button type="submit"> Submit Form </button></div>`
 
 		$("#app").html(formHTML)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		$("input, textarea").on ('focus', function(){
+			$(this).parents('div').addClass('focused')
+		}).on("blur", function(){
+			$(this).parents('div').removeClass('focused')
+		})
 	})
 })
+
